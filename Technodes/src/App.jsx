91febbf0d8814ef264027a2fe2db114b18d67593
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-06-18 19:43:33 +0530

    Setup Complete

---
+++ Technodes/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="bg-gray-900">
+      <h1 className="mt-9 text-center text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-400 to-red-600">
+        This is community website
+      </h1>
+      <p className="text-center text-2xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-pink-300 to-red-500">
+        under developement soon Lunch
+      </p>
+    </div>
+  );
+}

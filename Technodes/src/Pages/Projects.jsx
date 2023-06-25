b45d7dc0a8284543655aa7e177e0b2f3ad05ed3d
yash suthar <ysuthar8055@gmail.com>
2023-06-25 15:28:43 +0530

    added text for bettwe visiblity

--- Technodes/src/Pages/Projects.jsx
+++ Technodes/src/Pages/Projects.jsx
@@ -3,12 +3,15 @@ import ProjectCards from '../components/ProjectCards'
 
 function Projects() {
   return (
-    <div className='p-10 flex flex-wrap justify-center items-center'>
-        <div>
-            <ProjectCards/>
-            <ProjectCards/>
-            <ProjectCards/>
-            <ProjectCards/>
+    <div className="p-10 mt-20">
+        <p className='text-5xl font-bold text-center'>PROJECTS</p>
+        <div className='p-10 flex flex-wrap justify-center items-center'>\
+            <div>
+                <ProjectCards/>
+                <ProjectCards/>
+                <ProjectCards/>
+                <ProjectCards/>
+            </div>
         </div>
     </div>
   )

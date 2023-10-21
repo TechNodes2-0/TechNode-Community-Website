9910bded379e8ed4646ee01c207e6721b781b86a
yash suthar <ysuthar8055@gmail.com>
2023-10-21 21:45:03 +0530

    project cards updated

--- Technodes/src/Pages/Projects.jsx
+++ Technodes/src/Pages/Projects.jsx
@@ -1,19 +1,67 @@
 import React from 'react'
 import ProjectCards from '../components/ProjectCards'
 
+
 function Projects() {
+
+  const cardData = [
+    {
+      image:
+        "https://user-images.githubusercontent.com/85815172/272357345-a113ed3c-5611-4eb2-81b4-9896db64cfdc.png",
+      heading: "ElectiveHub",
+      description:
+        "ElectiveHub is an Education Management System designed to simplify and enhance the management of student information and elective subject selection in educational institutions. It offers a suite of applications for students, teachers, and administrators, providing a user-friendly platform.",
+      github: "https://github.com/TechNodes2-0/ElectiveHub",
+      link: "https://electivehub.onrender.com/",
+    },
+    {
+      image:
+        "",
+
+      heading: "Contentify",
+      description:
+        "Contentify is a versatile content creation, social media management, and educational resources platform. It provides users with a comprehensive set of tools to excel in content creation, streamline social media management, and access valuable educational resources.",
+      github: "https://github.com/TechNodes2-0/Contentify",
+      link: "https://contentifys.netlify.app/",
+    },
+    {
+      image:
+        "",
+
+      heading: "Laughhub",
+      description:
+        "Laugh Hub is a hilarious bot with advanced AI voice assistance that offers fun conversations, funny stories, a meme generator, and three to four entertaining games. It's the perfect destination for anyone looking for a good laugh and a break from their daily routine.",
+      github: "https://github.com/TechNodes2-0/Laughhub",
+      link: "https://laughhub.tech/",
+    },
+    {
+      image:
+        "",
+
+      heading: "CodeHUb",
+      description:
+        "CodeHubs is an online platform that fosters collaboration and learning among coding enthusiasts, particularly students. It serves as a hub for coding events, problem statements, and collaborative projects, providing a vibrant community for individuals passionate about programming.",
+      github: "https://github.com/TechNodes2-0/CodeHUb",
+      link: "https://codehubs.netlify.app/",
+    },
+  ];
+
   return (
     <div className="p-10 pt-20 bg-primary">
         <p className='text-3xl font-bold text-center'>PROJECTS</p>
-        <div className='p-10 flex flex-wrap justify-center items-center'>
-            <div>
-                <ProjectCards/>
-                <ProjectCards/>
-                <ProjectCards/>
-                <ProjectCards/>
+        <div className="flex justify-start flex-wrap max-w-4xl mx-auto">
+            {cardData.map(({ image, heading, description,github , link }) => (
+            <ProjectCards
+            key={heading}
+              image={image}
+              heading={heading}
+              description={description}
+              github={github}
+              link={link}
+            />
+          ))}
             </div>
         </div>
-    </div>
   )
 }
 
--- Technodes/src/components/ProjectCards.jsx
+++ Technodes/src/components/ProjectCards.jsx
@@ -1,20 +1,62 @@
-import React from 'react'
+import PropTypes from "prop-types";
 
-function ProjectCards() {
+function ProjectCards({ image, heading, description, github, link }) {
+  const updatedDesc = description.split(" ").slice(0, 10).join(" ");
   return (
-    <div className='my-20 transition duration-200 hover:scale-105 bg-primary'>
     <div>
-        <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-            <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="https://th.bing.com/th/id/OIP.F5GE1m5a2Ei8wY8loW-3AgHaHa?pid=ImgDet&rs=1" alt=""/>
-            <div className="flex flex-col justify-between p-4 leading-normal">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+      <div className=" w-64 m-4  border border-gray-200 rounded-lg shadow transition duration-200 hover:scale-105">
+        <div title="project image" className="bg-gray-800 border-0 rounded-t-lg">
+          <img className="rounded-t-lg object-cover h-48 mx-auto my-auto" src={!image? "https://blog.hubbado.com/content/images/2020/01/projectmanager.png": image} alt="image not available" />
+        </div>
+          <div className="flex flex-col justify-between h-40 px-5 py-2">
+            <div className="h-28 overflow-hidden">
+            <h5 className=" text-2xl font-bold tracking-tight">{heading}</h5>
+            <p className=" font-normal text-sm">{updatedDesc}...</p>
+
             </div>
-        </a>
+
+            <div className=" flex flex-row justify-start items-center my-2">
+              <div className=" font-bold transition duration-100 hover:scale-110">
+                <a href={github} title="github" className="  text-2xl  ">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    viewBox="0 0 496 512"
+                    className="fill-white hover:fill-gray-300"
+                  >
+                    <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                  </svg>
+                </a>
+              </div>
+              <div className=" font-bold mt-1 mx-3 transition duration-100 hover:scale-110">
+                <a
+                  href={link}
+                  title="live link"
+                  className= "  text-1xl font-semibold  " 
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="1em"
+                    viewBox="0 0 640 512"
+                    className="fill-white hover:fill-gray-300"
+                  >
+                    <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </div>
+      </div>
     </div>
-    
-    </div>
-  )
+  );
 }
 
-export default ProjectCards
+ProjectCards.propTypes = {
+  image: PropTypes.string.isRequired,
+  heading: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  github: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+};
+
+export default ProjectCards;
